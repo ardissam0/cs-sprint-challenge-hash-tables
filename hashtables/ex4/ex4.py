32a499fee4005aa77c230e889aed9713a374b2c8
Sam Ardis <ardis.productions@gmail.com>
2020-09-04 12:20:44 -0400

    Added ex4, tests are passing

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,6 +2,22 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
+    # create hash table (cache)
+    # create empty array as result
+    cache = {}
+    result = []
+
+    #loop through array
+    #add nums to cache
+    #uses absolute value
+    for num in a:
+        num = abs(num)
+        if cache.get(num) is None:
+            cache[num] = "Added to cache"
+
+        #already in cache
+        else:
+            result.append(num)
  
     return result
 
