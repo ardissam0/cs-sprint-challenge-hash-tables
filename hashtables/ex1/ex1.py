c440670db456e02b74e489926a45087911f125b2
Sam Ardis <ardis.productions@gmail.com>
2020-09-04 11:31:32 -0400

    Added ex1, tests passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,30 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # create cache
+    cache = {}
 
+    #create cases
+    #if length is less than or equal to 1
+    #its too short
+    if length <=1:
+        print("To short")
+        return None
+
+    #for i in the range of the length
+    #set the current weight
+    #if the current weight is in the cache
+    # previous equals the current
+    # otherwise cache the limit minus the weight as i
+    # find the limit
+    for i in range(length):
+        cur = weights[i]
+
+        if cur in cache:
+
+            previous = cache[cur]
+
+            return(i, previous)
+
+        cache[limit - weights[i]] = i
     return None
