bbd0cb43c16938b3b5366329d87593b631cc6b42
Sam Ardis <ardis.productions@gmail.com>
2020-09-04 12:42:49 -0400

    Added ex5, tests are passing

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -6,7 +6,32 @@ def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # intialize empty array
+    result = []
+
+    # create dict
+    dir = dict()
+
+    #loop through the files
+    for f in files:
+
+        #split files at /
+        s = f.split("/")
+        last = s[-1]
+
+        # create new entry into dict
+        if last not in dir:
+            dir[last] = []
+
+        # add curr path to dict with last as the key
+        dir[last].append(f)
+
+    # looop through each query
+    for q in queries:
+
+        # if the file ws found, add the paths
+        if q in dir:
+            result.extend(dir[q])
 
     return result
 
